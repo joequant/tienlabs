d35cddd662d89655b71dc124423f5737a42c681c
Joseph C Wang <joequant@gmail.com>
2019-10-11 13:48:07 +0800

    add moo items

---
+++ moo/install.sh
@@ -0,0 +1,16 @@
+set -e -v
+cat <<EOF >> /etc/dnf/dnf.conf
+fastestmirror=true
+excludepkgs=filesystem,chkconfig
+max_parallel_downloads=10
+EOF
+
+source /tmp/proxy.sh
+
+dnf upgrade --best --nodocs --allowerasing --refresh -y -x chkconfig -x filesystem
+dnf --setopt=install_weak_deps=False --best --allowerasing install -v -y --nodocs \
+    apache sudo wget gcc make byacc
+useradd user
+cp /tmp/startup.sh /home/user/startup.sh
+chmod a+x /home/user/statup.sh
+
