653e3df2d5d8eb364a6ebd33e4db8af4dc0f4d50
Joseph C Wang <joequant@gmail.com>
2019-10-20 07:06:31 +0800

    redo mayan with docker-compose

--- experiments/document-processing/mayan-bootstrap.sh
+++ experiments/document-processing/mayan-bootstrap.sh
@@ -5,34 +5,6 @@ set -e
 
 mkdir -p mayan
 mkdir -p postgres
-chmod a+rwx mayan postgres
-
-docker network create mayan
-
-docker pull mayanedms/mayanedms:latest
-docker pull postgres:12-alpine
-docker run -d \
-       --name mayan-edms-postgres \
-       --network=mayan \
-       --restart=always \
-       -e POSTGRES_USER=mayan \
-       -e POSTGRES_DB=mayan \
-       -e POSTGRES_PASSWORD=mayanuserpass \
-       -v `pwd`/postgres:/var/lib/postgresql/data \
-       postgres:12-alpine
-
-sleep 60
-
-docker run \
-       --name mayan-edms \
-       --restart=always \
-       --network=mayan \
-       -p 81:8000 \
-       -e MAYAN_DATABASE_ENGINE=django.db.backends.postgresql \
-       -e MAYAN_DATABASE_HOST=mayan-edms-postgres \
-       -e MAYAN_DATABASE_NAME=mayan \
-       -e MAYAN_DATABASE_PASSWORD=mayanuserpass \
-       -e MAYAN_DATABASE_USER=mayan \
-       -e MAYAN_DATABASE_CONN_MAX_AGE=0 \
-       -v `pwd`/mayan:/var/lib/mayan \
-       mayanedms/mayanedms:latest
+docker-compose up start_dependencies
+sleep 15
+docker-compose up mayan-edms
