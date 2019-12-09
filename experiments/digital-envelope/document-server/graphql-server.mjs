e9fbc6010cee863244dc9d9f2e31aae789e5ca79
Your Name <you@example.com>
2019-12-09 12:43:09 +0800

    add upload

--- experiments/digital-envelope/document-server/graphql-server.mjs
+++ experiments/digital-envelope/document-server/graphql-server.mjs
@@ -6,14 +6,43 @@ const { ApolloServer, gql } = apolloServer;
 const typeDefs = gql`
   type Query {
     hello: String
+    uploads: [File]	
   }
+
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
+
+  type Mutation {
+    singleUpload(file: Upload!): File!
+  }  
 `;
 
 // The root provides a resolver function for each API endpoint
 const resolvers = {
 Query: {
-  hello: () => 'Hello world!',
+  hello: () => {return 'Hello world!'},
+  uploads: () => {
+     // Return the record of files uploaded from your DB or API or filesystem.
   }
+},
+Mutation: {
+    async singleUpload(parent, { file }) {
+      const { stream, filename, mimetype, encoding } = await file;
+
+      // 1. Validate file metadata.
+
+      // 2. Stream file contents into cloud storage:
+      // https://nodejs.org/api/stream.html
+
+      // 3. Record the file upload in your DB.
+      // const id = await recordFile( … )
+
+      return { filename, mimetype, encoding };
+    }
+}
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
